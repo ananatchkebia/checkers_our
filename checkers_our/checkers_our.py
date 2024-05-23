dba5ed2755c0bae649d45fc9af7a29b8145ef0f7
AnaNachkebia <105235267+AnaNachkebia@users.noreply.github.com>
2024-05-24 03:49:09 +0400

    .

--- checkers_our/checkers_our.py
+++ checkers_our/checkers_our.py
@@ -1,6 +1,6 @@
 from pickle import NONE
 import pygame
-
+import copy
 WIDTH, HEIGHT = 500, 500
 ROWS, COLS = 8, 8
 SQUARE_SIZE = WIDTH//COLS 
@@ -136,7 +136,7 @@ class Board:
                 else:
                     print("emptySquare","     ",end='')
             print()            
-    def make_move(self,y1,x1,y2,x2,win):
+    def make_move(self,y1,x1,y2,x2,win = False):
         #If movement is legal than make move
         if self.is_legal_move(y1,x1,y2,x2):
            #Check if this movement is simple movement or killing opponent's piece
@@ -151,27 +151,34 @@ class Board:
                 if(y2>y1 and x2>x1):
                     self.board[y1+1][x1+1].square_color = BLACK
                     self.board[y1+1][x1+1].piece = None
-                    pygame.draw.rect(win, BLACK, ((x1+1) * SQUARE_SIZE,(y1+1) * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+                    if(win != False):
+                        pygame.draw.rect(win, BLACK, ((x1+1) * SQUARE_SIZE,(y1+1) * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
                 elif(y2>y1 and x2<x1):
                     self.board[y1+1][x1-1].square_color = BLACK
                     self.board[y1+1][x1-1].piece = None
-                    pygame.draw.rect(win, BLACK, ((x1-1) * SQUARE_SIZE,(y1+1) * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+                    if(win != False):
+                        pygame.draw.rect(win, BLACK, ((x1-1) * SQUARE_SIZE,(y1+1) * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
                 elif(y2<y1 and x2<x1):
                     self.board[y1-1][x1-1].square_color = BLACK
                     self.board[y1-1][x1-1].piece = None
-                    pygame.draw.rect(win, BLACK, ((x1-1) * SQUARE_SIZE,(y1-1) * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+                    if(win != False):
+                        pygame.draw.rect(win, BLACK, ((x1-1) * SQUARE_SIZE,(y1-1) * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
                 elif(y2<y1 and x2>x1):
                     self.board[y1-1][x1+1].square_color = BLACK
                     self.board[y1-1][x1+1].piece = None
-                    pygame.draw.rect(win, BLACK, ((x1+1) * SQUARE_SIZE,(y1-1) * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+                    if(win != False):
+                        pygame.draw.rect(win, BLACK, ((x1+1) * SQUARE_SIZE,(y1-1) * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
 
             #this is simple movement,so we just have to swap values on the board's following locations[y1][x1] and [y2][y1] 
             self.board[y2][x2], self.board[y1][x1] = self.board[y1][x1], self.board[y2][x2]
-            pygame.draw.rect(win, BLACK, (x1 * SQUARE_SIZE,y1 * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+            if(win != False):
+                pygame.draw.rect(win, BLACK, (x1 * SQUARE_SIZE,y1 * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
             if(self.board[y2][x2].piece.color == BLUE):
-                Piece(y2,x2,BLUE).draw(win)
+                if(win != False):
+                    Piece(y2,x2,BLUE).draw(win)
             if(self.board[y2][x2].piece.color == RED):
-                Piece(y2,x2,RED).draw(win)
+                if(win != False):
+                    Piece(y2,x2,RED).draw(win)
     def is_legal_move(self,y1,x1,y2,x2):
         for valid_square in self.valid_squares_to_move(y1,x1).values():
             if(valid_square[0] == y2 and valid_square[1] == x2):return True
@@ -180,86 +187,70 @@ class Board:
     #this function we need to control non-avoidance of possible kills
     #this means that if there is red's turn and on the current board exists movement
     #thet red piece can kill blue piece, then red can only make this movement
-    def possible_killing_positions(self,color):
-        killing_positions = []
-       
-    def kill_is_possible(self,color):
+    def sequential_kills_possible(self,color):
+        if self.kill_is_possible(color) != False:
+            #we must iterate on killing_positions and create copy of new board with these kills and check if in potential
+            #board still is possible make kill with the same piece
+            #but now we must take in mind the fact that after first killing valid moves will change
+            #piece can kill in backward as well as forward
+            for kill_position in self.kill_is_possible(color)[1]:
+                print("this is begining of ...")
+                valid_moves = self.valid_squares_to_move(kill_position[0],kill_position[1])
+                valid_moves = {key : value for key, value in valid_moves.items() if "Kill" in key}
+                temp_board = copy.deepcopy(self)
+                for moves in valid_moves.values():
+                    print(moves)
+                    temp_board.make_move(kill_position[0],kill_position[1],moves[0],moves[1])
+                    temp_board.board_representation()
+                    if(temp_board.kill_is_possible(color,False,moves[0],moves[1]) != False):
+                        print(temp_board.kill_is_possible(color,False,moves[0],moves[1])[1])
+                        
+
+    def kill_is_possible(self,color,first_kill = True,row = None,col = None):
         #if color is red,we should check possible moves for all red pieces and 
         #if killing is possible, then discover which piece can kill
         kill_is_possible = False
         killing_positions = []
-        for row in range(ROWS):
-            for col in range(COLS):
-                if(self.board[row][col].piece != None):
-                    if(self.board[row][col].piece.color == color):
-                        valid_squares = self.valid_squares_to_move(row,col)
-                        if "UpLeftKill" in valid_squares or "UpRightKill" in valid_squares or "DownRightKill" in valid_squares or "DownLeftKill" in valid_squares:
-                            kill_is_possible = True
-                            killing_positions.append([row,col])
+        if first_kill:
+            for row in range(ROWS):
+                for col in range(COLS):
+                    if(self.board[row][col].piece != None):
+                        if(self.board[row][col].piece.color == color):
+                            valid_squares = self.valid_squares_to_move(row,col)
+                            if "UpLeftKill" in valid_squares or "UpRightKill" in valid_squares or "DownRightKill" in valid_squares or "DownLeftKill" in valid_squares:
+                                kill_is_possible = True
+                                killing_positions.append([row,col])
+        if not first_kill:
+            if row-2>=0 and col+2<=7: 
+                if self.board[row-1][col+1].piece != None:
+                    if self.board[row-1][col+1].piece.color != color and self.board[row-2][col+2].piece == None:
+                        print("here we are 1")
+                        kill_is_possible = True
+                        killing_positions.append([row-2,col+2])
+            if row+2<=7 and col+2<=7: 
+                if self.board[row+1][col+1].piece != None:
+                    if self.board[row+1][col+1].piece.color != color and self.board[row+2][col+2].piece == None:
+                        print("here we are 2")
+                        kill_is_possible = True
+                        killing_positions.append([row+2,col+2])
+            if row+2<=7 and col-2>=0: 
+                if self.board[row+1][col-1].piece != None:
+                    if self.board[row+1][col-1].piece.color != color and self.board[row+2][col-2].piece == None:
+                        print("here we are 3")
+                        kill_is_possible = True
+                        killing_positions.append([row+2,col-2])
+            if row-2>=0 and col-2>=0: 
+                if self.board[row-1][col-1].piece != None:
+                    if self.board[row-1][col-1].piece.color != color and self.board[row-2][col-2].piece == None:
+                        print("here we are 4")
+                        kill_is_possible = True
+                        killing_positions.append([row-2,col-2])
         if(kill_is_possible): return True, killing_positions
         else: return False
         
-    #def valid_kills(self):
+#   def sequential_kill_possible(self,color):
         
-    '''
-    def is_legal_move(self,y1,x1,y2,x2):
-        #check that destination is black_square(so this square is empty and available)
-        if self.board[y2][x2].square_color != BLACK: 
-            print(y2+1," ",x2+1," is not BlackSquare") 
-            return False
-        #check that movement is valid for non-queen piece
-        if not self.legal_move_main_rule(y1,x1,y2,x2):return False
-        return True
-    def legal_move_main_rule(self,y1,x1,y2,x2):
-        #if piece is red than it can move up-right or up-left
-        if(self.board[y1][x1].piece.color == RED):
-            print("you want to move red piece!")
-            #move is not valid if it is not directed up with one or two squares
-            if(y1 - y2 != 1 and y1 - y2 != 2):
-                return False
-            if y1 - y2 == 1:
-                #move is not valid if difference between columns of previous and next positions is not equal to 1 
-                if(abs(x2 - x1) != 1):
-                    return False
-                else:
-                    return True
-            #here already means that y2-y1=2,so this will be valid only in case of killing piece of opponent
-            #so here we have to check two conditions:
-            #one that difference between columns of previous and next positions is equal to 2
-            #and second that between them is opponent's piece
-            else:
-                print("This is killing")
-                if(abs(x2 - x1) != 2):return False
-                if(x1 > x2):
-                    if(self.board[y1-1][x1-1].piece.color != BLUE):return False
-                else:
-                    if(self.board[y1-1][x1+1].piece.color != BLUE):return False
-                return True   
-        #if piece is black than it can move down-right or down-left
-        if(self.board[y1][x1].piece.color == BLUE):
-            print("you want to move Blue piece!")
-            #move is not valid if it is not directed down with one or two squares
-            if(y2 - y1 != 1 and y2 - y1 != 2):return False
-            if y2 - y1 == 1:
-                #move is not valid if difference between columns of previous and next positions is not equal to 1 
-                if(abs(x2 - x1) != 1):return False
-                return True
-            #here already means that y1-y2=2,so this will be valid only in case of killing piece of opponent
-            #so here we have to check two conditions:
-            #one that difference between columns of previous and next positions is equal to 2
-            #and second that between them is opponent's piece
-            else:
-                print("this is killing")
-                if(abs(x2 - x1) != 2): 
-                    return False
-                if(x1 > x2):
-                    if(self.board[y1+1][x1-1].piece.color != RED):
-                        return False
-                else:
-                    if(self.board[y1+1][x1+1].piece.color != RED):
-                        return False
-                return True
-    '''    
+    
 def main(): 
     
     run = True
@@ -267,6 +258,7 @@ def main():
     board = Board(WIN)
     click_list = []   
     potential_moves = []    
+    #We use movemement_count to discover who's turn is,if movement_count%2 == 0 then it's red's turn,otherwise blue's turn
     movement_count = 0
     click_count = 0
     list_for_possible_moves = []
@@ -277,18 +269,22 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
             if(pygame.mouse.get_pressed()[0]):
+                
                 movement_done = False
                 print("click count: ",click_count)
                 print("movement count: ",movement_count)
                 cl_row, cl_col = get_row_col_with_mouse_pos(pygame.mouse.get_pos())
                 #unda gavtesto click_count != 1
                 if(click_count == 0 and board.board[cl_row][cl_col].piece == None and not list_for_possible_moves):continue
-                if(board.board[cl_row][cl_col].piece != None):    
+                if(board.board[cl_row][cl_col].piece != None):
                     if(movement_count % 2 == 0  and board.board[cl_row][cl_col].piece.color != RED):continue
                 if(board.board[cl_row][cl_col].piece != None):
                     if(movement_count % 2 == 1  and board.board[cl_row][cl_col].piece.color != BLUE):continue
                     
-                if(board.board[cl_row][cl_col].piece != None ):
+    
+                if(board.board[cl_row][cl_col].piece != None):
+                    print("sequential kills method call: ")
+                    board.sequential_kills_possible(board.board[cl_row][cl_col].piece.color)
                     print("kill is possible: ",board.kill_is_possible(board.board[cl_row][cl_col].piece.color))                   
                     if(board.kill_is_possible(board.board[cl_row][cl_col].piece.color) != False):
                         print(board.kill_is_possible(board.board[cl_row][cl_col].piece.color))
@@ -297,10 +293,10 @@ def main():
                     else:killing = False
                         
 
-
                 print("list_for_possible_moves: ", list_for_possible_moves)
                 print("potential_moves: ",potential_moves)
                 
+                #amis shecvla momiwevs, vinaidan potential_moves shevcvli
                 if list_for_possible_moves:
                     if([cl_row,cl_col] in potential_moves):
                         for move in potential_moves:
@@ -344,6 +340,7 @@ def main():
                         valid_moves = board.valid_squares_to_move(cl_row,cl_col)
                         print(valid_moves)
                         
+                        #aq meqneba cvlilebebi gasaketebeli potential_moves cvlilebis gamo
                         #here I am trying to remove all potential moves except killing
                         if(killing):
                             valid_moves.clear()
